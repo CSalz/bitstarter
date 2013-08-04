533c6b06d81ff485a7d7023807a67017149f8856
Ubuntu <ubuntu@ip-172-31-28-232.us-west-2.compute.internal>
2013-08-04 15:21:42 +0000

    modify to include flow

--- web.js
+++ web.js
@@ -8,19 +8,19 @@ app.use(express.logger());
 app.get('/', function(request, response) {
 
     var lookup = path.basename(decodeURI(request.url)) ||'index.html', f = lookup;
-  
+
     fs.exists(f,function (exists) {
 	if (exists) {
 	    fs.readFile(f, function (err, data) {
 		if (err) {
 		    response.writeHead(500);
-		    response.end('Server Error!') 
+		    response.end('Server Error!')
 		    return;
 		}
 		var headers = {'Content-Type': 'text.html'}
 		response.writeHead(200 , {'Content-Type': 'text/html'});
 		response.end(data);
-	    })
+	    });
 	    return;
 	}
 	response.writeHead(404); // no file
@@ -29,7 +29,7 @@ app.get('/', function(request, response) {
 });
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
